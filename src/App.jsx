ef8f2258d2d6883beccaf5c46ddbb87a5582731d
Bablu Mehta <babloomehta895012@gmail.com>
2024-01-06 19:21:31 +0530

    hero section is complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import './App.css'
-import Navbar from './components/navbar/Navbar'
+import "./App.css";
+import Hero from "./components/hero/Hero.jsx";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
       <div>
-       <Navbar />
+        <Navbar />
+        <Hero />
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./hero.css";
+
+import HeroImg from "../../assets/hero-img.jpg";
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <img src={HeroImg} alt="Crowd of people at Club" />
+      <div className="hero-content">
+        <h3>New York</h3>
+        <p>The Atmosphere here at New York is lorem ipsum</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
