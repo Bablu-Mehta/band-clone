cf6ed2b8ba29ae57ab253f15ff55c41b433ca55d
Bablu Mehta <babloomehta895012@gmail.com>
2024-01-14 18:25:27 +0530

    BandRoute Page is Complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Booking from "./components/booking/Booking.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
 import Footer from "./components/footer/Footer.jsx";
-import Hero from "./components/bandRoute/hero/Hero.jsx";
-import Projects from "./components/bandRoute/projects/Projects.jsx";
-import About from "./components/bandRoute/about/About.jsx";
-import Contact from "./components/bandRoute/contact/Contact.jsx";
+import BandRoute from "./components/bandRoute/BandRoute.jsx";
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Navbar />
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/band" element={<About />} />
-    //     <Route path="/tour" element={<Booking />} />
-    //   </Routes>
-    //   <Footer />
-    // </BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/band" element={<BandRoute />} />
+        <Route path="/tour" element={<Booking />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
