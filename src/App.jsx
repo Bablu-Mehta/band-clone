742b022242402287c90ddb2822fe1df4540f4fa6
Bablu Mehta <babloomehta895012@gmail.com>
2024-01-16 12:04:14 +0530

    TourRoute's is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import Home from "./Home.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import About from "./components/about/About.jsx";
-import Booking from "./components/booking/Booking.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import BandRoute from "./components/bandRoute/BandRoute.jsx";
