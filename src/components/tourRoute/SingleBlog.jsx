dc7cfc294838ab5b429ca32600a1458db3f32ba4
Bablu Mehta <babloomehta895012@gmail.com>
2024-01-23 18:07:45 +0530

    done styling the cateringRoute page

--- src/components/tourRoute/SingleBlog.jsx
+++ src/components/tourRoute/SingleBlog.jsx
@@ -10,17 +10,17 @@ const SingleBlog = ({
   return (
     <div className="blog-card">
       <img src={blogImage} alt={blogHeading} />
-     <div className="blog-card__content">
-     <h2>{blogHeading}</h2>
-      <p className="blog-card__sub-heading">{blogSubHeading}</p>
-      <p className="blog-card__description">{blogDEscription}</p>
-      <div className="blog-card__buttons">
-        <p className="blog-card__btn">READ MORE</p>
-        <p className="blog-card__textOnly">
-          Comments <span>0</span>
-        </p>
+      <div className="blog-card__content">
+        <h2>{blogHeading}</h2>
+        <p className="blog-card__sub-heading">{blogSubHeading}</p>
+        <p className="blog-card__description">{blogDEscription}</p>
+        <div className="blog-card__buttons">
+          <p className="blog-card__btn">READ MORE</p>
+          <p className="blog-card__textOnly">
+            Comments <span>0</span>
+          </p>
+        </div>
       </div>
-     </div>
     </div>
   );
 };
