c5a4b296ea1c90295a2da2986f16eed9394bb8e8
Bablu Mehta <babloomehta895012@gmail.com>
2024-01-23 11:25:26 +0530

    cleanup the code in project section in bandRoute Page

--- src/components/bandRoute/projects/Projects.jsx
+++ src/components/bandRoute/projects/Projects.jsx
@@ -1,29 +1,21 @@
 import React from "react";
 import Project from "./Project";
 import "./projects.css";
-import summerHouse1 from "../../../assets/bandRoute/house5.jpg";
-import summerHouse2 from "../../../assets/bandRoute/house5.jpg";
-import brickHouse1 from "../../../assets/bandRoute/house2.jpg";
-import brickHouse2 from "../../../assets/bandRoute/house2.jpg";
-import renovated1 from "../../../assets/bandRoute/house3.jpg";
-import renovated2 from "../../../assets/bandRoute/house3.jpg";
-import barnHouse1 from "../../../assets/bandRoute/house4.jpg";
-import barnHouse2 from "../../../assets/bandRoute/house4.jpg";
+import { bandRouteProjectData } from "../../../constants";
 
 const Projects = () => {
+  const projects = bandRouteProjectData.map((project) => (
+    <Project
+      key={project.id}
+      projectImage={project.projectImage}
+      projectName={project.projectName}
+    />
+  ));
+
   return (
     <div className="band-projects-container">
       <h1>Projects</h1>
-      <section className="band-projects">
-        <Project projectImage={summerHouse1} projectName="Summer House" />
-        <Project projectImage={brickHouse1} projectName="Brick House" />
-        <Project projectImage={renovated1} projectName="Renovated" />
-        <Project projectImage={barnHouse1} projectName="Barn House" />
-        <Project projectImage={summerHouse2} projectName="Summer House" />
-        <Project projectImage={brickHouse2} projectName="Brick House" />
-        <Project projectImage={renovated2} projectName="Renovated" />
-        <Project projectImage={barnHouse2} projectName="Barn House" />
-      </section>
+      <section className="band-projects">{projects}</section>
     </div>
   );
 };
--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,11 @@ import janeDoe from "../src/assets/bandRoute/team1.jpg";
 import mikeRoss from "../src/assets/bandRoute/team3.jpg";
 import danStar from "../src/assets/bandRoute/team4.jpg";
 
+import summerHouse from "../src/assets/bandRoute/house5.jpg";
+import brickHouse from "../src/assets/bandRoute/house2.jpg";
+import renovated from "../src/assets/bandRoute/house3.jpg";
+import barnHouse from "../src/assets/bandRoute/house4.jpg";
+
 export const memberData = [
   {
     id: "m1",
@@ -35,5 +40,48 @@ export const memberData = [
     memberDesignation: "CEO & Founder",
     memberDescription:
       "Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.",
+  },
+];
+
+export const bandRouteProjectData = [
+  {
+    id: "p1",
+    projectImage: summerHouse,
+    projectName: "Summer House",
+  },
+  {
+    id: "p2",
+    projectImage: brickHouse,
+    projectName: "Brick House",
+  },
+  {
+    id: "p3",
+    projectImage: renovated,
+    projectName: "Renovated House",
+  },
+  {
+    id: "p4",
+    projectImage: barnHouse,
+    projectName: "Barn House",
+  },
+  {
+    id: "p5",
+    projectImage: summerHouse,
+    projectName: "Summer House",
+  },
+  {
+    id: "p6",
+    projectImage: brickHouse,
+    projectName: "Brick House",
+  },
+  {
+    id: "p7",
+    projectImage: renovated,
+    projectName: "Renovated House",
+  },
+  {
+    id: "p8",
+    projectImage: barnHouse,
+    projectName: "Barn House",
   },
 ];
